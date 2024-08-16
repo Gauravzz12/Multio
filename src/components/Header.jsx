1ebab4bc037f0a890557d3cde72d94fba9806590
Gauravzz12 <gaurav1234thakurgt@gmail.com>
2024-08-16 17:49:47 +0530

    chore: Minor Changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 function Header() {
   return (
-    <div className='header p-3 m-0 radius-20 bg-[#151515] text-white flex items-center justify-center '>
+    <div className='header p-3 m-0 radius-20 bg-[#151515] text-white flex items-center justify-center w-full '>
       
       <nav className='navbar p-3 relative z-30 text-2xl w-full flex justify-between items-center rounded-full bg-transparent '>
         <div className="title italic font-extrabold text-4xl font-mono mx-3 text-[#59c5a6] ">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,28 @@
 import React from "react";
 import { TypeWriterText } from "../components/UI/TypeWriterText";
-
+import controller from '../assets/images/controller.jpeg'
 function Home() {
   const textGroups = [
     [
       { text: "Welcome To", color: "text-white" },
-      { text: "Multio", color: "text-[#59c5a6]" }
+      { text: "Multio", color: "text-[#5cd2b9]" }
     ],
     [
       { text: "Your", color: "text-white" },
-      { text: "Multiplayer", color: "text-[#59c5a6]" }
+      { text: "Multiplayer", color: "text-[#5cd2b9]" }
     ],
     [
-      { text: "Gaming", color: "text-[#59c5a6]" },
+      { text: "Gaming", color: "text-[#5cd2b9]" },
       { text: "Destination", color: "text-white" }
     ]
   ];
 
   return (
-    <div className="container w-full h-screen pt-24 pl-24 bg-black text-white font-bold">
-      <div className="motto rounded w-full text-5xl flex flex-col bg-gradient-to-r from-black to-[#59c5a6]">
+    <div className="Container  m-0 w-full h-screen pt-16 pl-24 bg-black text-white font-bold flex flex-col ">
+      <div className="motto rounded text-5xl flex justify-between bg-gradient-to-r from-black from-20% to-[#5cd2b9]">
+        <div className=" flex flex-col items-center justify-center">
         {textGroups.map((group, groupIndex) => (
-          <div key={groupIndex} className="flex flex-row">
+          <div key={groupIndex} className="flex flex-row ">
             {group.map((item, itemIndex) => (
               <TypeWriterText
                 key={itemIndex}
@@ -31,6 +32,11 @@ function Home() {
             ))}
           </div>
         ))}
+
+        </div>
+          <div className="relative object-contain flex items-center p-2 mr-24">
+            <img src={controller} alt="" />
+          </div>
       </div>
       <div className="mt-8 text-green-800">
         <button aria-label="Play as Guest" className="px-4 py-2 bg-green-600 rounded hover:bg-green-700">
