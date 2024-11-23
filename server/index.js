45c50382b0c949dffb19312824fa525cd6f0efec
Gauravzz12 <gaurav1234thakurgt@gmail.com>
2024-11-23 23:39:14 +0530

    Testing 6

--- server/index.js
+++ server/index.js
@@ -12,12 +12,7 @@ const server = http.createServer(app);
 
 startApiServer(app);
 startSocketServer(server);
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "../client/dist/index.html"));
-  });
-}
+
 server.listen(API_PORT, () => {
   console.log(`Server running on port ${API_PORT}`);
 });
